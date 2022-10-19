a6fc6b5368673c6d7b5a2295321dd17043951b8f
Jeff Palm <jeff@jeffpalm.com>
2022-10-19 08:13:44 -0400

    update Wed Oct 19 08:13:44 EDT 2022

--- api/nearbydrivers.go
+++ api/nearbydrivers.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"strings"
 
 	"github.com/spudtrooper/goutil/request"
@@ -79,4 +80,61 @@ func (c *Client) NearbyDrivers(optss ...NearbyDriversOption) (*NearbyDriversInfo
 		return nil, err
 	}
 	return &res, nil
+}
+
+type VehicleView struct {
+	ID         string
+	ImageURL   string
+	Lat, Lng   float64
+	IsNearby   bool
+	JSON       string
+	DriverType string
+}
+
+func (n NearbyDriversInfo) VehicleViews() ([]VehicleView, error) {
+	var vehicleViews []VehicleView
+	for _, d := range n.NearbyDrivers {
+		id := d.ID
+		var jsonObj = struct {
+			NearbyDriver                       NearbyDriversInfoNearbyDriver                       `json:"nearby_drivers"`
+			NearbyDriverByStableOfferProductID NearbyDriversInfoNearbyDriverByStableOfferProductID `json:"nearby_drivers_by_stable_offer_product_id"`
+		}{
+			NearbyDriver: d,
+		}
+		var imageURL string
+		var isNearby bool
+		var lat, lng float64
+		var driverType string
+		for typ, nb := range n.NearbyDriversByStableOfferProductID {
+			for _, p := range nb.NearbyDriverProducts {
+				if id == p.DriverID {
+					jsonObj.NearbyDriverByStableOfferProductID = nb
+					isNearby = true
+					if len(d.Locations) > 0 {
+						lat = d.Locations[0].Lat
+						lng = d.Locations[0].Lng
+					}
+					if len(nb.MapMarkerImage.Sources) > 0 {
+						imageURL = nb.MapMarkerImage.Sources[0].URL
+					}
+					driverType = typ
+				}
+			}
+		}
+		jsonBytes, err := json.Marshal(jsonObj)
+		if err != nil {
+			return nil, err
+		}
+		jsonStr := string(jsonBytes)
+		vehicleViews = append(vehicleViews, VehicleView{
+			ID:         id,
+			ImageURL:   imageURL,
+			Lat:        lat,
+			Lng:        lng,
+			IsNearby:   isNearby,
+			JSON:       jsonStr,
+			DriverType: driverType,
+		})
+	}
+	return vehicleViews, nil
 }
--- render/nearbydrivers.go
+++ render/nearbydrivers.go
@@ -3,7 +3,6 @@ package render
 import (
 	"bytes"
 	_ "embed"
-	"encoding/json"
 	"sort"
 
 	"github.com/spudtrooper/lyft/api"
@@ -19,64 +18,13 @@ func NearbyDrivers(input any) ([]byte, handler.RendererConfig, error) {
 	config := handler.RendererConfig{
 		IsFragment: true,
 	}
-
-	type vehicleView struct {
-		ID         string
-		ImageURL   string
-		Lat, Lng   float64
-		IsNearby   bool
-		JSON       string
-		DriverType string
+	vehicleViews, err := params.VehicleViews()
+	if err != nil {
+		return nil, config, err
 	}
-	var vehicleViews []vehicleView
-
-	for _, d := range params.NearbyDrivers {
-		id := d.ID
-		var jsonObj = struct {
-			NearbyDriver                       api.NearbyDriversInfoNearbyDriver                       `json:"nearby_drivers"`
-			NearbyDriverByStableOfferProductID api.NearbyDriversInfoNearbyDriverByStableOfferProductID `json:"nearby_drivers_by_stable_offer_product_id"`
-		}{
-			NearbyDriver: d,
-		}
-		var imageURL string
-		var isNearby bool
-		var lat, lng float64
-		var driverType string
-		for typ, nb := range params.NearbyDriversByStableOfferProductID {
-			for _, p := range nb.NearbyDriverProducts {
-				if id == p.DriverID {
-					jsonObj.NearbyDriverByStableOfferProductID = nb
-					isNearby = true
-					if len(d.Locations) > 0 {
-						lat = d.Locations[0].Lat
-						lng = d.Locations[0].Lng
-					}
-					if len(nb.MapMarkerImage.Sources) > 0 {
-						imageURL = nb.MapMarkerImage.Sources[0].URL
-					}
-					driverType = typ
-				}
-			}
-		}
-		jsonBytes, err := json.Marshal(jsonObj)
-		if err != nil {
-			return nil, config, err
-		}
-		jsonStr := string(jsonBytes)
-		vehicleViews = append(vehicleViews, vehicleView{
-			ID:         id,
-			ImageURL:   imageURL,
-			Lat:        lat,
-			Lng:        lng,
-			IsNearby:   isNearby,
-			JSON:       jsonStr,
-			DriverType: driverType,
-		})
-	}
-
 	sort.Slice(vehicleViews, func(i, j int) bool { return vehicleViews[i].ID < vehicleViews[j].ID })
 	var data = struct {
-		VehicleViews []vehicleView
+		VehicleViews []api.VehicleView
 	}{
 		VehicleViews: vehicleViews,
 	}
